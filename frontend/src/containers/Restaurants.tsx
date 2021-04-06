7c2c0f21744b157ea639d462f7256e5bcb84aa7c
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-04-06 09:49:01 +0900

    Add test fetchAPI

--- frontend/src/containers/Restaurants.tsx
+++ frontend/src/containers/Restaurants.tsx
@@ -1,6 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { fetchRestaurants } from '../apis/restaurants';
 
 export const Restaurants: FC = () => {
+  useEffect(() => {
+    fetchRestaurants().then((data) => console.log(data))
+  }, []);
+
   return (
     <>
       レストラン一覧
